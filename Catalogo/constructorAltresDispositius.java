7efc3ebe9758acfdab7ab5bfbfd34098bb342f19
luis <lcornejo20@jaumebalmes.net>
2023-09-27 15:33:42 +0200

    vercion inicial de altres dispositius

---
+++ Catalogo/constructorAltresDispositius.java
@@ -0,0 +1,42 @@
+public class constructorAltresDispositius {
+    String marca = " ";
+    String modelo = " ";
+    int PreuBase =  0;
+    String  descripcio = " ";
+
+    public constructorAltresDispositius() {
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getPreuBase() {
+        return PreuBase;
+    }
+
+    public void setPreuBase(int PreuBase) {
+        this.PreuBase = PreuBase;
+    }
+
+    public String getDescripcio() {
+        return descripcio;
+    }
+
+    public void setDescripcio(String descripcio) {
+        this.descripcio = descripcio;
+    }
+    
+}
